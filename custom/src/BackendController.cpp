0041ef9fd40cfa78c476804b65f97675dc7b4557
Ryan <25047695+Ryanf55@users.noreply.github.com>
2026-08-13 16:29:24 -0600

    Allow ground scan (#44)
    Signed-off-by: Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>

--- custom/src/BackendController.cpp
+++ custom/src/BackendController.cpp
@@ -451,6 +451,7 @@ void BackendController::processTelemetryUpdates()
 
                             this->setResumeMissionButtonEn(false);
                             this->setSendGoalButtonEn(true);
+                            this->setStartScanButtonEn(true);
                         }
                     } 
                     //ToDo: I left the code below for allowing single UAV flight. However, I will need to make additional changes to get 
